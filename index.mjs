733950d49a31825919594dfc51ce0b1dcd6f36c7
Agustin E. Duarte | UAA-Sistemas <aeduarte@uaa.com.ar>
2025-08-08 11:59:04 -0300

    Commit

--- index.mjs
+++ index.mjs
@@ -2,47 +2,20 @@ import { cwd } from "process"
 import { CSV } from "./models/csv.mjs"
 import { crearCsv } from './config/csv_init_.mjs';
 
-const main = async() =>{
-const fileName = await crearCsv();
-
-const csv = new CSV('csvs', fileName)
-
-    try{
-        await csv.agregar('1', 'Ojotas', 43, 5000);
-        await csv.agregar('2', 'Ojotas', 43, 5000);
-        await csv.agregar('3', 'Ojotas', 43, 5000);
-        await csv.agregar('4', 'Ojotas', 43, 5000);
-
-        console.log('Productos agregados con éxito!');
-
-        console.log('\nAntes de borrar: ')
-        console(await csv.leer());
-
-        await csv.borrar('2');
-        console.log('\nDespués de borrar: ');
-        console.log(await csv.leer());
-    } catch {
-        console.log('Algo mal hiciste pa');
-    }
-
-}
+const main = async () => {
+  const fileName = await crearCsv(); 
 
 
+  const csv = new CSV('csvs', fileName);
 
-/*(async () =>{
-    try{
-        const contenido = await csv.leer();
-        console.log(contenido);
+  await csv.agregar('1', 'Camisa', 10, 20.5);
 
-        await csv.agregar('3', 'Ojotas', 43, 5000);
-        console.log('Producto agregado con éxito!');
+  const contenido = await csv.leer();
+  console.log('\nContenido del CSV:');
+  console.log(contenido);
+};
 
-        const archivos = await csv.listar();
-        console.log('Archivos CSV: ', archivos);
-    } catch {
-        console.log('Algo mal hiciste pa');
-    }
-})(); */
+
 
 main();
 
